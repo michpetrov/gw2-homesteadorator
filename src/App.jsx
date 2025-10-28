92a384fdd3f59a17d161a9df3a3078cb585a1a69
Michal Petrov <mpetrov@redhat.com>
2025-10-28 17:38:42 +0100

    add GitHub link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import Navigation from './Navigation.jsx'
 import Notice from './Notice.jsx'
+import GithubCorner from './GithubCorner.jsx'
 import './App.css'
 
 export default () => {
 
   return (
+    <>
+    <GithubCorner />
     <div className="content">
       <Notice />
       <h1>GW2 Homesteadorator</h1>
@@ -14,5 +17,6 @@ export default () => {
         <Navigation />
       </footer>
     </div>
+    </>
   )
 }
